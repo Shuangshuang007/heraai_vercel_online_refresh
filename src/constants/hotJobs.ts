9909641a31a8ca70863a1376af641bdce010ada6
Shuangshuang007 <shuangshuang.516.wu@gmail.com>
2025-10-09 11:02:36 +1100

    Add 4 new job title options: Christmas Casual, Casual Jobs, Part-time Jobs, Seasonal Work

--- src/constants/hotJobs.ts
+++ src/constants/hotJobs.ts
@@ -93,7 +93,11 @@ export const HOT_JOBS_CONFIG = {
     'Therapist',
     'Social Worker',
     'Graduate Program',
-    'Internship Program'
+    'Internship Program',
+    'Christmas Casual',
+    'Casual Jobs',
+    'Part-time Jobs',
+    'Seasonal Work'
   ],
   
   // 模糊匹配关键词
